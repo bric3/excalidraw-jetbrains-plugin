dd588f625f65c24c7675d7b510c6169b20dc1ddf
Brice Dutheil <brice.dutheil@gmail.com>
2021-06-20 22:56:51 +0200

    feat(build): gulp can now ship multiple files

--- excalidraw-assets/gulpfile.js
+++ excalidraw-assets/gulpfile.js
@@ -1,13 +1,30 @@
 // Ideas from https://www.labnol.org/code/bundle-react-app-single-file-200514
 
-const { task, src, dest } = require('gulp');
+const path = require('path');
+const del = require('del');
+const {task, series, src, dest} = require('gulp');
 const replace = require('gulp-replace')
 const inlineSource = require('gulp-inline-source-html');
 
-task('default', () => {
+task('clean', () => {
+    return del('build/gulp-dist', {force:true});
+});
+
+task('inline-html', () => {
     return src('build/react-build/*.html')
         .pipe(replace('.js"></script>', '.js" inline></script>'))
         .pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
-        .pipe(inlineSource({ compress: false }))
+        .pipe(inlineSource({compress: false}))
         .pipe(dest('build/gulp-dist'))
 })
+
+task('copy-map-files', () => {
+    return src('build/react-build/**/*.map')
+        .pipe(dest(function (file) {
+            // flatten directory structure
+            file.path = path.join(file.base, path.basename(file.path));
+            return 'build/gulp-dist';
+        }))
+});
+
+task('default', series('clean', 'inline-html', 'copy-map-files'));
