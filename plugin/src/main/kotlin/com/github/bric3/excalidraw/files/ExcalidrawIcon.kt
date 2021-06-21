3774d4d0bd9ce01607155135f6dd50035f4481b1
Brice Dutheil <brice.dutheil@gmail.com>
2021-06-21 22:32:49 +0200

    chore(plugin): Try new file icons

--- plugin/src/main/kotlin/com/github/bric3/excalidraw/files/ExcalidrawIcon.kt
+++ plugin/src/main/kotlin/com/github/bric3/excalidraw/files/ExcalidrawIcon.kt
@@ -3,5 +3,5 @@ package com.github.bric3.excalidraw.files
 import com.intellij.openapi.util.IconLoader
 
 object ExcalidrawIcon {
-    val ICON = IconLoader.getIcon("/icons/excalidraw-file-alt-icon.svg", this.javaClass)
+    val ICON = IconLoader.getIcon("/icons/excalidraw-file-sketch-icon.svg", this.javaClass)
 }
