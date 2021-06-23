f93e9d550aa1447dbbfc412d01090e4c5c0e0e83
Brice Dutheil <brice.dutheil@gmail.com>
2021-06-23 23:39:53 +0200

    feat(plugin): Declares excalidraw files as json

--- plugin/src/main/kotlin/com/github/bric3/excalidraw/files/ExcalidrawFileType.kt
+++ plugin/src/main/kotlin/com/github/bric3/excalidraw/files/ExcalidrawFileType.kt
@@ -1,12 +1,14 @@
 package com.github.bric3.excalidraw.files
 
-import com.github.bric3.excalidraw.Excalidraw
+import com.intellij.json.JsonLanguage
 import com.intellij.openapi.fileTypes.LanguageFileType
 
 /**
  * Identify Excalidraw file types
  */
-object ExcalidrawFileType : LanguageFileType(Excalidraw) {
+object ExcalidrawFileType : LanguageFileType(JsonLanguage.INSTANCE, true)
+//                            , OSFileIdeAssociation // uncomment when icons are ready
+{
     override fun getName() = "Excalidraw sketch"
     override fun getDescription() = "Excalidraw sketch file"
     override fun getDefaultExtension() = "excalidraw"
