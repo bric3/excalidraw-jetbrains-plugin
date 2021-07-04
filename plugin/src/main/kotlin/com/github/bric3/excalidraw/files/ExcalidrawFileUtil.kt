de0593b362cf32cc605c7854187709a5017acb10
Brice Dutheil <brice.dutheil@gmail.com>
2021-07-05 01:25:21 +0200

    chore(plugin): Cleanup dead approach for extracting Excalidraw Scene from SVG

--- plugin/src/main/kotlin/com/github/bric3/excalidraw/files/ExcalidrawFileUtil.kt
+++ plugin/src/main/kotlin/com/github/bric3/excalidraw/files/ExcalidrawFileUtil.kt
@@ -1,11 +1,7 @@
 package com.github.bric3.excalidraw.files
 
 
-import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.JsonDeserializer
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.intellij.openapi.diagnostic.logger
@@ -28,9 +24,9 @@ class ExcalidrawFileUtil private constructor() {
     companion object {
         val EXCALIDRAW_EMBEDDED_SCENE = Key<ByteArray>("application/vnd.excalidraw+json")
 
-        val logger = logger<ExcalidrawFileUtil>()
+        private val logger = logger<ExcalidrawFileUtil>()
         
-        val mapper = jacksonObjectMapper().apply {
+        private val mapper = jacksonObjectMapper().apply {
             configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         }
 
@@ -43,41 +39,14 @@ class ExcalidrawFileUtil private constructor() {
                 // To be aligned with plugin.xml
                 arrayOf(".excalidraw", ".excalidraw.json").any { ext -> file.name.endsWith(ext) } -> true
 
-                // TODO disabled for now, as long as parsing the binary payload in the embedded json don't work
                 // check if SVG file has a comment with 'payload-type:application/vnd.excalidraw+json'
                 file.name.endsWith(".svg") -> {
-                    // TODO put scene in file user data
-                    try {
-                        file.inputStream.reader().use {
-                            val factory: XMLInputFactory = XMLInputFactory.newInstance()
-                            val eventReader: XMLEventReader = factory.createXMLEventReader(it)
-                            while (eventReader.hasNext()) {
-                                val event: XMLEvent = eventReader.nextEvent()
-
-                                if (event.eventType == XMLStreamConstants.COMMENT) {
-                                    val text: String = (event as Comment).text
-                                    if (text.contains("payload-type:application/vnd.excalidraw+json")) {
-                                        return true
-                                    }
-                                }
-                            }
-                        }
-                    } catch (ioe: IOException) {
-                        logger.warn("Unable to read this SVG file, maybe it is malformed, file: '$file'", ioe)
-                    } catch (xse: XMLStreamException) {
-                        logger.warn("Unable to read this SVG file, maybe it is malformed, file: '$file'", xse)
-                    }
-                    false
+                    svgHasEmbeddedScene(file)
                 }
 
                 // check if PNG file has a tEXt chunk with keyword 'application/vnd.excalidraw+json'
                 file.name.endsWith("png") -> {
-                    val sceneFromPng = tryExtractExcalidrawSceneFromPng(file)
-                    if (sceneFromPng != null) {
-//                        file.putUserDataIfAbsent(EXCALIDRAW_EMBEDDED_SCENE, sceneFromPng)
-                        return true
-                    }
-                    return false
+                    pngHasEmbeddedScene(file)
                 }
 
 
@@ -103,7 +72,33 @@ class ExcalidrawFileUtil private constructor() {
             }
         }
 
-        fun tryExtractExcalidrawSceneFromPng(
+        private fun pngHasEmbeddedScene(file: VirtualFile) = tryReadPngExcalidrawScene(file) != null
+
+        private fun svgHasEmbeddedScene(file: VirtualFile): Boolean {
+            try {
+                file.inputStream.reader().use {
+                    val factory: XMLInputFactory = XMLInputFactory.newInstance()
+                    val eventReader: XMLEventReader = factory.createXMLEventReader(it)
+                    while (eventReader.hasNext()) {
+                        val event: XMLEvent = eventReader.nextEvent()
+
+                        if (event.eventType == XMLStreamConstants.COMMENT) {
+                            val text: String = (event as Comment).text
+                            if (text.contains("payload-type:application/vnd.excalidraw+json")) {
+                                return true
+                            }
+                        }
+                    }
+                }
+            } catch (ioe: IOException) {
+                logger.warn("Unable to read this SVG file, maybe it is malformed, file: '$file'", ioe)
+            } catch (xse: XMLStreamException) {
+                logger.warn("Unable to read this SVG file, maybe it is malformed, file: '$file'", xse)
+            }
+            return false
+        }
+
+        private fun tryReadPngExcalidrawScene(
             file: VirtualFile,
         ): ByteArray? {
             try {
@@ -133,141 +128,6 @@ class ExcalidrawFileUtil private constructor() {
             } catch (ioe: IOException) {
                 logger.warn("Unable to read this PNG file, maybe it is malformed, file: '$file'", ioe)
             }
-
-            return null
-        }
-
-        // TODO find a way
-        fun tryExtractExcalidrawSceneFrom(svgContent: String): String {
-            // basically reimplements decodeSvgMetadata
-            // https://github.com/excalidraw/excalidraw/blob/5c73c5813ce92d3c7b0610530f78ccb06a47d983/src/data/image.ts#L106-L136
-
-            // The file should be already verified to contain the following excalidraw payload type
-            // payload-type:application/vnd.excalidraw+json
-
-            // const match = svg.match(/<!-- payload-start -->(.+?)<!-- payload-end -->/);
-            val base64Payload =
-                svgContent.substringAfter("<!-- payload-start -->").substringBefore("<!-- payload-end -->")
-
-
-            // const versionMatch = svg.match(/<!-- payload-version:(\d+) -->/);
-            // const version = versionMatch?.[1] || "1";
-            // const isByteString = version !== "1";
-
-            val payloadVersion = when (val mr = Regex("<!-- payload-version:(\\d+) -->").find(svgContent)) {
-                null -> "1"
-                else -> mr.groupValues[1]
-            }
-            val isByteString = payloadVersion != "1"
-
-            // const json = await base64ToString(match[1], isByteString);
-            //   isByteString ? atob(base64) : byteStringToString(atob(base64));
-            //    atob : ASCII to Binary
-            //    byteStringToString : new TextDecoder("utf-8").decode(byteStringToArrayBuffer(byteString))
-            //
-            //    byteStringToArrayBuffer : (byteString: string) => {
-            //                                const buffer = new ArrayBuffer(byteString.length);
-            //                                const bufferView = new Uint8Array(buffer);
-            //                                for (let i = 0, len = byteString.length; i < len; i++) {
-            //                                  bufferView[i] = byteString.charCodeAt(i);
-            //                                }
-            //                                return buffer;
-            //                              };
-            //
-            //    TextDecocer.decode : decode(ab: ArrayBuffer) {
-            //                           return new Uint8Array(ab).reduce(
-            //                             (acc, c) => acc + String.fromCharCode(c),
-            //                             "",
-            //                           );
-            //                         }
-
-            if (!isByteString) {
-                throw IllegalArgumentException("payload version 1 not supported at this time")
-            }
-
-            // for payload version > 1
-//            val jsonString = Base64.decode(base64Payload)
-
-            val bytes = java.util.Base64.getDecoder().decode(base64Payload)
-
-            // const encodedData = JSON.parse(json);
-            val encodedData = mapper.readValue<EncodedPayload>(bytes)
-
-
-            // if (!("encoded" in encodedData)) {
-            //   // legacy, un-encoded scene JSON
-            //   if (
-            //     "type" in encodedData &&
-            //     encodedData.type === EXPORT_DATA_TYPES.excalidraw
-            //   ) {
-            //     return json;
-            //   }
-            //   throw new Error("FAILED");
-            // }
-            if (encodedData.encoded == null) {
-                // legacy not handled yet
-                throw IllegalArgumentException("legacy non encoded payload not supported at this time")
-            }
-
-            // return await decode(encodedData);
-            // export const decode = async (data: EncodedData): Promise<string> => {
-            //  let decoded: string;
-            //
-            //  switch (data.encoding) {
-            //    case "bstring":
-            //      // if compressed, do not double decode the bstring
-            //      decoded = data.compressed
-            //        ? data.encoded
-            //        : await byteStringToString(data.encoded);
-            //      break;
-            //    default:
-            //      throw new Error(`decode: unknown encoding "${data.encoding}"`);
-            //  }
-            //
-            //  if (data.compressed) {
-            //    return inflate(new Uint8Array(byteStringToArrayBuffer(decoded)), {
-            //      to: "string",
-            //    });
-            //  }
-            //
-            //  return decoded;
-            //};
-
-            val compressed = encodedData.compressed
-
-            val decoded: ByteArray = when (val encoding = encodedData.encoding) {
-                "bstring" -> encodedData.encoded
-                else -> throw IllegalArgumentException("decode: unknown encoding \"${encoding}\"")
-            }
-            val map: String = decoded.let {
-                if (compressed == true) {
-                    // inflate(new Uint8Array(byteStringToArrayBuffer(decoded)), {
-                    //     to: "string",
-                    // })
-                    //                        return inflated;
-                    ""
-                } else {
-                    //                        return byteStringToString(data.encoded);
-                    ""
-                }
-            }
-
-            return map
-        }
-    }
-
-    data class EncodedPayload(
-        val version: String?,
-        val encoding: String?,
-        val compressed: Boolean?,
-//        @JsonDeserialize(using = ByteArraySerializer::class)
-        val encoded: ByteArray?
-    )
-
-    class ByteArraySerializer : JsonDeserializer<ByteArray?>() {
-        override fun deserialize(jp: JsonParser, dc: DeserializationContext): ByteArray? {
-//            val binaryValue = jp.binaryValue
-            val node: JsonNode = jp.codec.readTree(jp)
 
             return null
         }
--- plugin/src/test/kotlin/com/github/bric3/excalidraw/files/ExcalidrawFileUtilTest.kt
+++ plugin/src/test/kotlin/com/github/bric3/excalidraw/files/ExcalidrawFileUtilTest.kt
@@ -6,7 +6,6 @@ import com.intellij.testFramework.LightVirtualFile
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import org.assertj.core.api.Assertions.assertThat
 import org.intellij.lang.annotations.Language
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.nio.charset.StandardCharsets
 
@@ -158,32 +157,6 @@ class ExcalidrawFileUtilTest : BasePlatformTestCase() {
     fun should_not_accept_malformed_json_file() {
         val malformedJson = """ { "malformed_json: "whatever" }"""
         assertThat(isExcalidrawFile(virtualFile("malformed.json", malformedJson))).isFalse
-    }
-
-
-    @Test
-    @Disabled("Blocked on scene extraction from SVG")
-    fun should_decode_scene_from_svg_with_embedded_excalidraw_payload() {
-        @Language("xml") val svgWithExcalidrawScene =
-            """
-            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 46" width="128" height="92">
-            <!-- svg-source:excalidraw -->
-            <!-- payload-type:application/vnd.excalidraw+json --><!-- payload-version:2 --><!-- payload-start -->eyJ2ZXJzaW9uIjoiMSIsImVuY29kaW5nIjoiYnN0cmluZyIsImNvbXByZXNzZWQiOnRydWUsImVuY29kZWQiOiJ4nGVSyU7DMFx1MDAxML3zXHUwMDE1kbmyJCVd4FbKKiQkVCEkXHUwMDEwXHUwMDA3k0xcdTAwMTOLqW3ZXHUwMDEz2oL4d2ynxKH4YGnebG/ezNdekjDaaGBnXHSDdcFRlIav2IHHP8BYoaRzXHKCbVVjilx1MDAxMFlcdTAwMTNpe3Z8XHUwMDFjM45cbrVss1x1MDAwMGFcdJKsi3txdpJ8hd95ROlz76aj6+dy+Fx1MDAwNlx1MDAwZlirw1x1MDAxM13cPTazkFx1MDAxYYJ+yVx1MDAxMKwpomtcdTAwMDeN01Fnb5ydXHKzzl6JkmqH5XlcdTAwMDfVIKqaPPmYxmWFvnraIZaMeoeZQmV81/00vNj4jVx1MDAxN++VUY0su1x1MDAxODJcXFrNjVx1MDAxYjLGLVx1MDAwNOKcNtjKw4u6McB2ujxtSWY7eJdnlVx1MDAxMzNmubZVLcHaPzlK80JQXHUwMDEwII1zeI76tlxmqr/u1q+50ds6zHqjx1xmwG9lPFx1MDAxOI5O8skgXHUwMDE2jMtcdTAwMWbvYvdKhjPITvM8XHUwMDFi5JM0ii7shds/hZpcdTAwMGKOXHUwMDE2opBew8v2NlqeskGMa/frdvRuXHUwMDAwUfVkVZLm4tN363Hz6Fx1MDAxNV9cbtz8UcbXmKKoPGmGsOitxzEn4W61c5PS/SVbQCHDTJNcdTAwMDB+uz/IyLjWc+Lkfe0hO6lFueVcdTAwMTRHYFx1MDAxZlx1MDAwMlbn/49lf1x1MDAxMVx1MDAxZdvzNb9/XHUwMDAw4FnnXG4ifQ==<!-- payload-end -->
-            <defs>
-              <style>
-                @font-face {
-                  font-family: "Virgil";
-                  src: url("https://excalidraw.com/Virgil.woff2");
-                }
-                @font-face {
-                  font-family: "Cascadia";
-                  src: url("https://excalidraw.com/Cascadia.woff2");
-                }
-              </style>
-            </defs>
-            <rect x="0" y="0" width="64" height="46" fill="#ffffff"></rect><g transform="translate(10 10) rotate(0 22 13)"><text x="0" y="18" font-family="Virgil, Segoe UI Emoji" font-size="20px" fill="#000000" text-anchor="start" style="white-space: pre;" direction="ltr">Hello</text></g></svg>
-            """
-        println(ExcalidrawFileUtil.tryExtractExcalidrawSceneFrom(svgWithExcalidrawScene.trimIndent()))
     }
 
     private fun virtualFile(fileName: String, content: String): LightVirtualFile =
