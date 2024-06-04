417b8e77fdbfd7abc9d5230993323302913b43a7
Brice Dutheil <brice.dutheil@gmail.com>
2024-06-05 00:55:37 +0200

    feature: Enable excalidraw icon for OS integration

--- plugin/src/main/kotlin/com/github/bric3/excalidraw/files/ExcalidrawFileType.kt
+++ plugin/src/main/kotlin/com/github/bric3/excalidraw/files/ExcalidrawFileType.kt
@@ -2,14 +2,13 @@ package com.github.bric3.excalidraw.files
 
 import com.github.bric3.excalidraw.ExcalidrawJson
 import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.fileTypes.OSFileIdeAssociation
 import icons.ExcalidrawIcons
 
 /**
  * Identify Excalidraw file types
  */
-object ExcalidrawFileType : LanguageFileType(ExcalidrawJson, true)
-//                            , OSFileIdeAssociation // uncomment when icons are ready
-{
+object ExcalidrawFileType : LanguageFileType(ExcalidrawJson, true), OSFileIdeAssociation {
     override fun getName() = "Excalidraw sketch"
     override fun getDescription() = "Excalidraw sketch file"
     override fun getDefaultExtension() = "excalidraw"
