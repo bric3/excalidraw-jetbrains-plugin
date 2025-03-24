fbd7565ce8ec6542e55fb9238c32ecd5bba02c2f
Brice Dutheil <brice.dutheil@gmail.com>
2025-03-24 09:09:43 +0100

    chore: Tabs vs spaces

--- excalidraw-embedded-app/vite.config.ts
+++ excalidraw-embedded-app/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+    plugins: [preact()],
     define: {
         "process.env.IS_PREACT": JSON.stringify("true"),
     },
